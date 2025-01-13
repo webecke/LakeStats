60542c68774be3a2ec561053cbff66d44677fa94
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-01-12 22:16:46 -0700

    test frontend update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+        <h1>TEST</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
