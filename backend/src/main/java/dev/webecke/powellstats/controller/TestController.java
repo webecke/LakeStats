460dbde78276ea190ac7c6176e3c1a1c582fb968
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-01-15 14:10:26 -0700

    gradle adjustments

--- backend/src/main/java/dev/webecke/powellstats/controller/TestController.java
+++ backend/src/main/java/dev/webecke/powellstats/controller/TestController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/1")
-    public String getCurrentConditions() {
-        return "Oh hey, this is a test! And you got a string!";
+    public String test1() {
+        return "Oh hey, this is a test! And you got a string!!!!";
+    }
+    @GetMapping("/2")
+    public String test2() {
+        return "Hmmmmmm, this seems to be some sort of test. I wonder whats going on.";
     }
 }
